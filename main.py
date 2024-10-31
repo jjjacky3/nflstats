2ed0c1a69e4daa078764f46613d4e0d2ca7a137a
jjjacky3 <610jackzhang@gmail.com>
2024-10-30 23:18:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -31,6 +31,8 @@ def main():
 
         st.subheader("How many rows are there in the dataset?", divider=True)
         st.subheader("How many passing yards gained by SEA in the game vs SF?", divider=True)
+        st.subheader("who is Seahawk's quarter back?", divider=True)
+        st.subheader("who in Seahawk has the most penalty by yards?", divider=True)
 
 
 if __name__ == "__main__":
