76d4ba9f909b1b4350a81b45ad2ce1426243f4a1
jjjacky3 <610jackzhang@gmail.com>
2024-10-30 21:55:11 -0700

    remove unused import

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import os
 import streamlit as st
 
-from dotenv import load_dotenv
-
 from langchain.agents.agent_types import AgentType
 from langchain_experimental.agents.agent_toolkits import create_csv_agent
 from langchain_openai import ChatOpenAI, OpenAI
